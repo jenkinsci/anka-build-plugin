1bacf5f92df2b9682667099429192c5ac7ac164c
asaf <asaf.gu6@gmail.com>
2017-07-24 14:42:40 +0300

    added the missing exceptions directoy

---
+++ src/main/java/com/exceptions/AnkaMgmtException.java
@@ -0,0 +1,11 @@
+package com.veertu.ankaMgmtSdk.exceptions;
+
+/**
+ * Created by asafgur on 18/05/2017.
+ */
+public class AnkaMgmtException extends Exception {
+
+    public AnkaMgmtException(Throwable e) {
+        super(e);
+    }
+}
