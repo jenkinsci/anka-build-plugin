bcd55ad2fba6e24075f229847eb528db827d90cd
Alex Fishman <alex.fishman@gmail.com>
2018-08-07 15:34:35 +0300

    add java logging
    Signed-off-by: Alex Fishman <alex.fishman@gmail.com>

--- src/main/java/com/veertu/plugin/anka/JnlpCommandBuilder.java
+++ src/main/java/com/veertu/plugin/anka/JnlpCommandBuilder.java
@@ -5,6 +5,23 @@ import jenkins.slaves.JnlpSlaveAgentProtocol;
 
 public class JnlpCommandBuilder {
 
+    static private String scriptTemplate =
+            "echo \"Initiating startup script\" > log.txt\n" +
+            "if [ ! -f agent.jar ]; then\n" +
+            "   echo \"downloading jar\" >> log.txt\n" +
+            "   CURL=$(curl --fail -O \"%s/jnlpJars/agent.jar\" --output agent.jar 2>&1)\n" +
+            "   if [ $? -ne 0 ]; then\n" +
+            "       CURL=$(curl --fail -O \"%s/jnlpJars/agent.jar\" --output agent.jar 2>&1)\n" +
+            "       if [ $? -ne 0 ]; then\n" +
+            "           echo $CURL | grep --quiet 'The requested URL returned error: 404' >> log.txt\n" +
+            "           exit 1\n" +
+            "       fi\n" +
+            "   fi\n" +
+            "fi\n" +
+            "echo \"Executing java command: %s\" >> log.txt\n" +
+            "%s &> log.txt\n";
+
+
     public static String makeCommand(String nodeName) {
         String effectiveJenkinsUrl = Jenkins.getInstance().getRootUrl();
         String format = "java -jar agent.jar -jnlpUrl %s/computer/%s/slave-agent.jnlp -secret %s";
@@ -13,21 +30,11 @@ public class JnlpCommandBuilder {
     }
 
     public static String makeStartUpScript(String nodeName) {
-        String scriptTemplate = "echo \"Initiating startup script\" > log.txt\n" +
-                "if [ ! -f agent.jar ]; then\n" +
-                "\techo \"downloading jar\" >> log.txt\n" +
-                "\tCURL=$(curl --fail -O \"%s/jnlpJars/agent.jar\" --output agent.jar 2>&1)\n" +
-                "\tif [ $? -ne 0 ]; then\n" +
-                "\t\tCURL=$(curl --fail -O \"%s/jnlpJars/agent.jar\" --output agent.jar 2>&1)\n" +
-                "\t\tif [ $? -ne 0 ]; then\n" +
-                "        \t\techo $CURL | grep --quiet 'The requested URL returned error: 404' >> log.txt\n" +
-                "\t\t\texit 1\n" +
-                "        \tfi\n" +
-                "\tfi\n" +
-                "fi\n";
+
         String jarCommand = makeCommand(nodeName);
-        String script = String.format(scriptTemplate, Jenkins.getInstance().getRootUrl(), Jenkins.getInstance().getRootUrl());
-        return script + jarCommand + ">> log.txt";
+        String script = String.format(scriptTemplate, Jenkins.getInstance().getRootUrl(),
+                Jenkins.getInstance().getRootUrl(), jarCommand, jarCommand);
+        return script;
 
     }
 }
