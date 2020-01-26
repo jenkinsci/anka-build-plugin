193666bddfc9f221265376808a1b4bb987d01d9c
asafg6 <asaf_gurrr@hotmail.com>
2020-01-26 16:40:33 +0200

    made dynamic slave work synchronously

--- src/main/java/com/veertu/plugin/anka/DynamicSlave.java
+++ src/main/java/com/veertu/plugin/anka/DynamicSlave.java
@@ -1,15 +1,18 @@
 package com.veertu.plugin.anka;
 
-import antlr.ANTLRException;
 import com.veertu.ankaMgmtSdk.AnkaAPI;
 import com.veertu.ankaMgmtSdk.AnkaMgmtVm;
 import com.veertu.ankaMgmtSdk.exceptions.AnkaMgmtException;
 import hudson.model.Descriptor;
-import hudson.model.Label;
+import hudson.plugins.sshslaves.SSHLauncher;
 import hudson.slaves.ComputerLauncher;
+import hudson.slaves.EnvironmentVariablesNodeProperty;
+import hudson.slaves.JNLPLauncher;
 import hudson.slaves.NodeProperty;
+import jenkins.slaves.RemotingWorkDirSettings;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -40,6 +43,104 @@ public class DynamicSlave extends AnkaOnDemandSlave {
         //     e.printStackTrace();
         //     throw new AnkaMgmtException(e);
         // }
-        return AnkaOnDemandSlave.createProvisionedSlave(cloud.getAnkaApi(), template);
+        if (template.getLaunchMethod().toLowerCase().equals(LaunchMethod.SSH)) {
+            return createSSHSlave(cloud.getAnkaApi(), template);
+        } else if (template.getLaunchMethod().toLowerCase().equals(LaunchMethod.JNLP)) {
+            return createJNLPSlave(cloud.getAnkaApi(), template);
+        }
+        return null;
     }
+
+
+    protected static AnkaOnDemandSlave createJNLPSlave(AnkaAPI ankaAPI, AnkaCloudSlaveTemplate template) throws AnkaMgmtException, IOException, Descriptor.FormException {
+//        AnkaMgmtCloud.Log("vm %s is booting...", vm.getId());
+        String nodeName = generateName(template);
+        String jnlpCommand = JnlpCommandBuilder.makeStartUpScript(nodeName, template.getExtraArgs(), template.getJavaArgs(), template.getJnlpJenkinsOverrideUrl());
+
+        AnkaMgmtVm vm = ankaAPI.makeAnkaVm(
+                template.getMasterVmId(), template.getTag(), template.getNameTemplate(), template.getSSHPort(), jnlpCommand, template.getGroup(), template.getPriority());
+        try {
+            vm.waitForBoot(template.getSchedulingTimeout());
+        } catch (InterruptedException| IOException|AnkaMgmtException e) {
+            vm.terminate();
+            throw new AnkaMgmtException(e);
+        }
+        AnkaMgmtCloud.Log("vm %s %s is booted, creating jnlp launcher", vm.getId(), vm.getName());
+
+        String tunnel = "";
+        JNLPLauncher launcher = new JNLPLauncher(template.getJnlpTunnel(),
+                "",
+                RemotingWorkDirSettings.getEnabledDefaults());
+        ArrayList<EnvironmentVariablesNodeProperty.Entry> a = new ArrayList<EnvironmentVariablesNodeProperty.Entry>();
+        for (AnkaCloudSlaveTemplate.EnvironmentEntry e :template.getEnvironments()) {
+            a.add(new EnvironmentVariablesNodeProperty.Entry(e.name, e.value));
+        }
+
+        EnvironmentVariablesNodeProperty env = new EnvironmentVariablesNodeProperty(a);
+        ArrayList<NodeProperty<?>> props = new ArrayList<>();
+        props.add(env);
+
+        AnkaMgmtCloud.Log("launcher created for vm %s %s", vm.getId(), vm.getName());
+        AnkaOnDemandSlave slave = new AnkaOnDemandSlave(nodeName, template.getTemplateDescription(), template.getRemoteFS(),
+                template.getNumberOfExecutors(),
+                template.getMode(),
+                template.getLabelString(),
+                launcher,
+                props, template, vm);
+        slave.setDisplayName(vm.getName());
+        return slave;
+    }
+
+    protected static AnkaOnDemandSlave createSSHSlave(AnkaAPI ankaAPI, AnkaCloudSlaveTemplate template) throws InterruptedException, AnkaMgmtException, IOException, Descriptor.FormException {
+        AnkaMgmtVm vm = ankaAPI.makeAnkaVm(
+                template.getMasterVmId(), template.getTag(), template.getNameTemplate(), template.getSSHPort(), null, template.getGroup(), template.getPriority());
+        try {
+
+            ArrayList<EnvironmentVariablesNodeProperty.Entry> a = new ArrayList<EnvironmentVariablesNodeProperty.Entry>();
+            for (AnkaCloudSlaveTemplate.EnvironmentEntry e : template.getEnvironments()) {
+                a.add(new EnvironmentVariablesNodeProperty.Entry(e.name, e.value));
+            }
+
+            EnvironmentVariablesNodeProperty env = new EnvironmentVariablesNodeProperty(a);
+            ArrayList<NodeProperty<?>> props = new ArrayList<>();
+            props.add(env);
+
+            AnkaOnDemandSlave slave = new AnkaOnDemandSlave(vm.getId(), template.getTemplateDescription(), template.getRemoteFS(),
+                    template.getNumberOfExecutors(),
+                    template.getMode(),
+                    template.getLabelString(),
+                    null,
+                    props, template, vm);
+            AnkaMgmtCloud.Log("vm %s is booting...", vm.getId());
+            try {
+                vm.waitForBoot(template.getSchedulingTimeout());
+            } catch (InterruptedException| IOException|AnkaMgmtException e) {
+                vm.terminate();
+                throw new AnkaMgmtException(e);
+            }
+            AnkaMgmtCloud.Log("vm %s %s is booted, creating ssh launcher", vm.getId(), vm.getName());
+            SSHLauncher launcher = new SSHLauncher(vm.getConnectionIp(), vm.getConnectionPort(),
+                    template.getCredentialsId(),
+                    template.getJavaArgs(), null, null, null, launchTimeoutSeconds, maxNumRetries, retryWaitTime, null);
+
+
+
+            slave.setLauncher(launcher);
+
+            AnkaMgmtCloud.Log("launcher created for vm %s %s", vm.getId(), vm.getName());
+            String name = vm.getName();
+            slave.setNodeName(name);
+            return slave;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            vm.terminate();
+            throw e;
+        }
+    }
+
 }
+
+
+
+
