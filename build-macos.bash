d80d4f35259bd07e20e35b70c37d9f9fc5a9a299
Nathan Pierce <norsegaud@gmail.com>
2025-02-05 10:20:48 -0600

    quick fix for ci scripts and bulding on mac

---
+++ build-macos.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -exo pipefail
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $SCRIPT_DIR/_ci
+docker build -t anka-build-plugin .
+cd $SCRIPT_DIR
+docker run --rm --name anka-build-plugin -it -v ${SCRIPT_DIR}:/home/ubuntu/anka-build-plugin -v ${HOME}/.mvn:/home/ubuntu/.mvn anka-build-plugin /bin/bash -c "cd /home/ubuntu/anka-build-plugin && mvn -Dmaven.repo.local=\${HOME}/.mvn package"
