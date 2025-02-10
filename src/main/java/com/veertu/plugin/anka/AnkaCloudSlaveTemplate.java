cd89705092406df664729e37e427d8623bf16421
Nathan Pierce <norsegaud@gmail.com>
2025-02-10 12:16:49 -0600

    design cleanup

--- src/main/java/com/veertu/plugin/anka/AnkaCloudSlaveTemplate.java
+++ src/main/java/com/veertu/plugin/anka/AnkaCloudSlaveTemplate.java
@@ -1,31 +1,41 @@
 package com.veertu.plugin.anka;
 
-import com.cloudbees.plugins.credentials.common.StandardUsernameCredentials;
-import com.cloudbees.plugins.credentials.common.StandardUsernameListBoxModel;
-import com.cloudbees.plugins.credentials.domains.SchemeRequirement;
-import com.veertu.ankaMgmtSdk.AnkaVmTemplate;
-import com.veertu.ankaMgmtSdk.NodeGroup;
-import com.veertu.plugin.anka.exceptions.AnkaHostException;
-import hudson.Extension;
-import hudson.model.*;
-import hudson.model.labels.LabelAtom;
-import hudson.security.ACL;
-import hudson.security.AccessControlled;
-import hudson.util.ListBoxModel;
-import hudson.Util;
-import hudson.util.FormValidation;
-import jenkins.model.Jenkins;
-import net.sf.json.JSONObject;
-import org.kohsuke.stapler.*;
-
-import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.logging.Logger;
 
+import javax.annotation.Nullable;
+
+import org.kohsuke.stapler.AncestorInPath;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.QueryParameter;
+import org.kohsuke.stapler.StaplerRequest;
+
 import static com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials;
+import com.cloudbees.plugins.credentials.common.StandardUsernameCredentials;
+import com.cloudbees.plugins.credentials.common.StandardUsernameListBoxModel;
+import com.cloudbees.plugins.credentials.domains.SchemeRequirement;
+import com.veertu.ankaMgmtSdk.AnkaVmTemplate;
+import com.veertu.ankaMgmtSdk.NodeGroup;
+import com.veertu.plugin.anka.exceptions.AnkaHostException;
+
+import hudson.Extension;
+import hudson.model.AbstractDescribableImpl;
+import hudson.model.Computer;
+import hudson.model.Describable;
+import hudson.model.Descriptor;
+import hudson.model.ItemGroup;
+import hudson.model.Label;
+import hudson.model.Node;
+import hudson.model.labels.LabelAtom;
+import hudson.security.ACL;
+import hudson.security.AccessControlled;
+import hudson.util.ListBoxModel;
+import jenkins.model.Jenkins;
+import net.sf.json.JSONObject;
 
 /**
  * Created by avia on 10/07/2016.
@@ -272,6 +282,9 @@ public class AnkaCloudSlaveTemplate extends AbstractSlaveTemplate implements Des
 
         public ListBoxModel doFillMasterVmIdItems(@QueryParameter String cloudName) {
             AnkaMgmtCloud cloud = (AnkaMgmtCloud) Jenkins.get().getCloud(cloudName);
+            // java.util.logging.Logger logger = java.util.logging.Logger.getLogger("jenkins.system.log");
+            // logger.info("cloudName: " + cloudName);
+            // logger.info("cloud: " + cloud);
             ListBoxModel models = new ListBoxModel();
             models.add("Choose Vm template", "");
             if (! cloud.isOnline()) {
